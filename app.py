c78649a260000dacb214b6da3c58aac6f2bb0b03
neelesh <134229599+AI-ML-PROBO-TEAM@users.noreply.github.com>
2023-09-23 13:47:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -157,4 +157,4 @@ def process_input():
     return jsonify(error='Input value not provided'), 400
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=8080)  # Change 8080 to the desired port number
+    app.run(debug=False,host='0.0.0.0',port=8080)  # Change 8080 to the desired port number
